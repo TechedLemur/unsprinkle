f24b5c8c69d28a62319fdae8cc769a4f7a8eb716
Jostein Hjorland Tysse <jostein.h.tysse@gmail.com>
2023-12-14 16:35:56 +0100

    Finish workshop

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type="image/avif"
+            srcSet={`
+            ${src.replace(".jpg", ".avif")} 1x,
+            ${src.replace(".jpg", "@2x.avif")} 2x,
+            ${src.replace(".jpg", "@3x.avif")} 3x
+            `}
+          />
+          <source
+            type="image/jpg"
+            srcSet={`
+            ${src} 1x,
+            ${src.replace(".jpg", "@2x.jpg")} 2x,
+            ${src.replace(".jpg", "@3x.jpg")} 3x
+            `}
+          />
+
+          <Image src={src} alt={alt} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,20 +47,29 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  overflow: hidden;
+  white-space: nowrap;
+  max-width: 100%;
+  text-overflow: ellipsis;
+  padding: 4px 0;
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
+  /* line-height: 1.5rem; */
+  margin-right: 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  /* overflow: hidden; */
+  /* width: fit-content; */
+  /* text-overflow: ellipsis; */
 `;
 
 export default PhotoGridItem;
